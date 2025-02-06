6948648a7a81cfbc6054bf2844b82ebf208ef194
Prithvirajsinh07 <pgdodiya7208@gmail.com>
2025-02-06 12:08:54 +0530

    commit massage

---
+++ Prectice/Api intigration/api/src/componnents/Home.jsx
@@ -0,0 +1,48 @@
+import axios from 'axios';
+import React, { useState } from 'react'
+
+const Home = () => {
+  const [data , setData] = useState({
+    productName : "",
+    category : "" ,
+    price : ""
+  });
+
+  function handleSubmite (e) {
+    e.preventDefault();
+    console.log(data);
+    setData({
+      productName: "",
+      category: "",
+      price: "",
+    });
+createData();
+alert("you product add successfully")
+  }
+  async function createData() {
+    axios.post("http://localhost:5000/product" , data);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmite} action="">
+      <label  htmlFor="">Name :-</label>
+        <input
+        value={data.productName}
+         onChange={(e)=> {
+setData({...data , productName : e.target.value})
+        }} type="text" /><br /><br />
+        <label htmlFor="">Brand :-</label>
+        <input   value={data.category} onChange={(e)=> {
+setData({...data , category : e.target.value})
+        }}  type="text" /><br /><br />
+        <label htmlFor="">Price :-</label>
+        <input   value={data.price} onChange={(e)=> {
+setData({...data , price : e.target.value})
+        }}  type="number" /><br /><br />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default Home
---
+++ Project/Telwind/telwind/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './componnets/Navbar'
+import Section from './componnets/Section'
+import Footer from './componnets/Footer'
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Section/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
